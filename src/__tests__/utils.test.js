8a074769fbc3c5a27f4fb6da849071686a38aa74
sean810 <seantashel@gmail.com>
2024-11-19 13:26:36 +0300

    Complete tab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,9 +1,29 @@
+// src/__tests__/utils.test.js
 import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
+    // Arrange
+    const currentYear = new Date().getFullYear();
     const birthYear = 1984;
+    const expectedAge = currentYear - birthYear;
+
+    // Act
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+
+    // Assert
+    expect(ageOfPerson).toEqual(expectedAge);
+  });
+
+  it("returns 0 for a person born this year", () => {
+    // Arrange
+    const currentYear = new Date().getFullYear();
+    const birthYear = currentYear;
+
+    // Act
+    const ageOfPerson = currentAgeForBirthYear(birthYear);
+
+    // Assert
+    expect(ageOfPerson).toBe(0);
   });
 });
